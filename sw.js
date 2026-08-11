a8056a6bbcc1db0c2c05ad62f37aa5834cc94a6f
bahaa90231-art <bahaa90231@gmail.com>
2026-08-11 22:08:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,149 @@
+const CACHE_NAME = "baha-academy-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.webmanifest"
+];
+
+
+/* ==============================
+   INSTALL
+============================== */
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(
+                    APP_FILES
+                );
+
+            })
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+/* ==============================
+   ACTIVATE
+============================== */
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys
+                    .filter(
+                        key =>
+                            key !== CACHE_NAME
+                    )
+                    .map(
+                        key =>
+                            caches.delete(key)
+                    )
+
+                );
+
+            })
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+/* ==============================
+   FETCH
+============================== */
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if(
+            event.request.method !== "GET"
+        ){
+
+            return;
+
+        }
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+            .then(cachedResponse => {
+
+                if(cachedResponse){
+
+                    return cachedResponse;
+
+                }
+
+                return fetch(
+                    event.request
+                )
+                .then(response => {
+
+                    if(
+                        !response ||
+                        response.status !== 200 ||
+                        response.type === "opaque"
+                    ){
+
+                        return response;
+
+                    }
+
+                    const responseClone =
+                    response.clone();
+
+                    caches.open(
+                        CACHE_NAME
+                    )
+                    .then(cache => {
+
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+
+                    });
+
+                    return response;
+
+                })
+                .catch(() => {
+
+                    return caches.match(
+                        "./index.html"
+                    );
+
+                });
+
+            })
+
+        );
+
+    }
+);
